50b182f033b837b663ef56b81f75a2e7da441ab7
Kallies? <128437288+kalliesxd@users.noreply.github.com>
2023-03-21 03:49:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTA2OTU2NTQwNjU3MTkyNTUwNA.GUl4RW.aLFGL08UJOxaUQTCg2Nq29rwkyu94m5pPStLOw", 
+    token: "MTA2OTU2NTQwNjU3MTbak-bak-şuan-aldın", 
     prefix: "."
 }
 
